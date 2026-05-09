2dbed91c30eb5d4ffc279239de89ab90fc30ff93
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-09 16:14:46 +0300

    QA Automation

---
+++ app.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as playwright:
+    #launch the browser
+    browser = playwright.firefox.launch(headless=False)
+    #create a new page
+    page = browser.new_page()
+    #visit playwright page
+    page.goto("https://playwright.dev/")
+
+    #locate a link element with Docs text and click on it
+    docs_button = page.get_by_role('link', name='Docs')
+    docs_button.click()
+
+    # Get the URL
+    print("Docs:", page.url)
+
+    browser.close()
