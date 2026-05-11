20450a22ffe9bdc1da5538a435fbf034f3ff8568
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-11 09:22:19 +0300

    QA Automation

--- Playwright/config/config.py
+++ Playwright/config/config.py
@@ -8,5 +8,10 @@ base_url = "https://example.com"
 username = "username"
 password = "test123"
 
+BROWSER = "chromium"
+TIMEOUT = 30000
+HEADLESS = False
+
+
 # The above code can be called by importing it. You keep this one and create another .py file for your test
 
