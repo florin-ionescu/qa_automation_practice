2163785535955700d7ef7e66521668b5b50d06bc
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-19 20:38:16 +0300

    Updated

--- Pytest/test_strings.py
+++ Pytest/test_strings.py
@@ -1,1 +1,14 @@
 #work in progress
+
+"""
+self.assertEqual(actual, expected)      # actual == expected
+self.assertNotEqual(actual, expected)   # actual != expected
+self.assertTrue(value)                  # value is True
+self.assertFalse(value)                 # value is False
+self.assertIsNone(value)                # value is None
+self.assertIsNotNone(value)             # value is not None
+self.assertIn(item, list_or_text)       # item exists inside something
+self.assertNotIn(item, list_or_text)    # item does not exist
+self.assertGreater(a, b)                # a > b
+self.assertLess(a, b)                   # a < b
+"""
