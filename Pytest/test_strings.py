60fd50627782b358a09326e0ddb6a0529f0dcb9e
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-19 20:41:00 +0300

    Test strings learning

--- Pytest/test_strings.py
+++ Pytest/test_strings.py
@@ -1,4 +1,3 @@
-#work in progress
 
 """
 self.assertEqual(actual, expected)      # actual == expected
@@ -12,3 +11,11 @@ self.assertNotIn(item, list_or_text)    # item does not exist
 self.assertGreater(a, b)                # a > b
 self.assertLess(a, b)                   # a < b
 """
+
+def test_string_equal():
+    name = "Florin"
+    assert name == "Florin"
+
+def test_string_contains():
+    message = "Welcome to Python testing"
+    assert "Python" in message
