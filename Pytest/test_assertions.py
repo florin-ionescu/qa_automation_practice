04000055a6ea0a10aeb46b4b0c38a16b025e16ee
Florin Ionescu <126186642+Cichicean@users.noreply.github.com>
2026-05-20 07:38:56 +0300

    Practiced assert

--- Pytest/test_assertions.py
+++ Pytest/test_assertions.py
@@ -1,1 +1,38 @@
-#work in progress
+
+
+def test_equal():
+    assert True
+def test_not_equal():
+    assert False
+
+def test_equal():
+    assert 5==5
+
+def test_true():
+    is_logged_in = True
+    assert is_logged_in
+
+def test_false():
+    has_error = False
+    assert not has_error
+
+def test_greater_than():
+    assert 5>5
+
+def test_less_than():
+    assert 4<5
+
+def test_in_text():
+    message = "I am learning Python Testing now"
+    assert "Python" in message
+
+def test_not_in_text():
+    message = "I am learning Python Testing now"
+    assert "Java" not in message
+
+def test_is_not_none():
+    name = "Florin"
+    assert name is not None
+def test_none():
+    x = None
+    assert x is None
